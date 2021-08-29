53267fa6a08499f12633e0244652e4661b0c72bc
Your Name <you@example.com>
2021-08-29 23:09:46 +0530

    Added : UC5

--- EmpWage.js
+++ EmpWage.js
@@ -63,3 +63,19 @@ console.log(" Hour :" +totalEmpHrs+ " Emp Wage :" +empWage);
     let empWage = totalEmpHrs* WAGE_PER_HOUR;
     console.log(" UC4 - Total Hrs : " +totalEmpHrs+ "Emp Wage:" +empWage);
     }
+
+// #UC5 While Loop
+{
+    const MAX_HRS_IN_MONTH = 160;
+    const NUM_OF_WORKING_DAYS = 20;
+    let totalEmpHrs=0;
+    let totalWorkingDays = 0;
+    while(totalEmpHrs<= MAX_HRS_IN_MONTH && 
+        totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
+            let empCheck = Math.floor(Math.random() * 10) %3 ;
+            totalEmpHrs += getWorkingHours(empCheck);
+        }
+        let empWage = totalEmpHrs * WAGE_PER_HOUR;
+        console.log(" UC5 - Total Days : " +totalEmpHrs+ "Emp Wage :" +empWage);
+}
