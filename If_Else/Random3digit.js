9a46cdb4cf1d146512014d7560379319ed0d9106
Your Name <you@example.com>
2021-08-31 16:46:10 +0530

    If Else Learning Problems

---
+++ If_Else/Random3digit.js
@@ -0,0 +1,14 @@
+// Program to find 3 digit random number below 999
+var minNum = 999;
+var maxNum = 0;
+
+for (var i=1; i<=5; i++){
+    var num = Math.floor(Math.random()*(999-100+1)+100);
+    if(minNum > num){
+        minNum = num;
+    }
+    if(maxNum < num){
+        maxNum = num;
+    }
+}
+console.log(maxNum, minNum)
---
+++ If_Else/flipcoin.js
@@ -0,0 +1,10 @@
+// program to get Head & Tails
+
+var num = Math.floor((Math.random() * 2) + 1);
+
+if(num == 1){
+    console.log("Heads");
+}
+else{
+    console.log("Tails");
+}
